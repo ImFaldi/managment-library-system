ff1acb4e4e13dfaa3481ff1001d6ebfba28452d8
rifaldibpn2 <rifaldibpn2@gmail.com>
2023-06-22 04:11:08 +0700

    Api Data

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,45 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\BookController;
+use App\Http\Controllers\API\AuthorController;
+use App\Http\Controllers\API\CategoryController;
+use App\Http\Controllers\API\BorrowController;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
     Route::post('register', 'register');
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
+});
+
+Route::middleware('auth:api')->group(function () {
+    // Books
+    Route::get('books', [BookController::class, 'index']);
+    Route::post('books', [BookController::class, 'store']);
+    Route::get('books/{id}', [BookController::class, 'show']);
+    Route::put('books/{id}', [BookController::class, 'update']);
+    Route::delete('books/{id}', [BookController::class, 'destroy']);
+
+    // Authors
+    Route::get('authors', [AuthorController::class, 'index']);
+    Route::post('authors', [AuthorController::class, 'store']);
+    Route::get('authors/{id}', [AuthorController::class, 'show']);
+    Route::put('authors/{id}', [AuthorController::class, 'update']);
+    Route::delete('authors/{id}', [AuthorController::class, 'destroy']);
+
+    // Categories
+    Route::get('categories', [CategoryController::class, 'index']);
+    Route::post('categories', [CategoryController::class, 'store']);
+    Route::get('categories/{id}', [CategoryController::class, 'show']);
+    Route::put('categories/{id}', [CategoryController::class, 'update']);
+    Route::delete('categories/{id}', [CategoryController::class, 'destroy']);
+
+    // Borrow
+    Route::get('borrow', [BorrowController::class, 'index']);
+    Route::post('borrow', [BorrowController::class, 'store']);
+    Route::get('borrow/{id}', [BorrowController::class, 'show']);
+    Route::put('borrow/{id}', [BorrowController::class, 'update']);
+    Route::delete('borrow/{id}', [BorrowController::class, 'destroy']);
+
 });

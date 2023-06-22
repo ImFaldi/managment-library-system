2477d99cfb5c96ab4cc2b31d3565ca07f42e5a59
rifaldibpn2 <rifaldibpn2@gmail.com>
2023-06-23 03:34:37 +0700

    Package

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -17,7 +16,6 @@ use Inertia\Inertia;
 |
 */
 
-
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -26,8 +24,6 @@ Route::get('/', function () {
         'phpVersion' => PHP_VERSION,
     ]);
 });
-
-Route::post('/login-store', [AuthController::class, 'login'])->name('login.store');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -39,4 +35,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';

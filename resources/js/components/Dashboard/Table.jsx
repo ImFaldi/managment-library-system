4c25c3e58e90178d446628e2b75cc5b4637f65a9
rifaldibpn2 <rifaldibpn2@gmail.com>
2023-06-27 17:53:08 +0700

    Modal

--- resources/js/components/Dashboard/Table.jsx
+++ resources/js/components/Dashboard/Table.jsx
@@ -1,8 +1,13 @@
-import { Link } from '@inertiajs/react';
-
 function Table({ title, columns, rows }) {
-
+    const openModal = (index) => {
+        window.selectedRowIndex = index;
+        const modal = document.getElementById('my_modal');
+        if (modal) {
+            modal.showModal();
+        }
+    };
     return (
+
         <div className="card bg-base-100 shadow-xl h-min w-full mr-5">
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
@@ -39,10 +44,23 @@ function Table({ title, columns, rows }) {
                                         </div>
                                     </td>
                                     <td className="text-sm">
-                                    <span className="badge badge-success badge-md text-white">{row.role}</span>
+                                        <span className="badge badge-success badge-md text-white">{row.role}</span>
                                     </td>
                                     <td>{row.phone}</td>
-                                    <td><button className="btn btn-info btn-sm text-white">Detail</button></td>
+                                    <td>{/* You can open the modal using ID.showModal() method */}
+                                        <button className="btn btn-info btn-sm text-white" onClick={() => openModal(index)}>Detail</button>
+                                        <dialog id="my_modal" className="modal">
+                                            <form method="dialog" className="modal-box">
+                                                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                                                <h3 className="font-bold text-lg">Hello!</h3>
+                                                <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                                                <div className="modal-action">
+                                                    <button className="btn btn-error btn-sm text-white">Delete</button>
+                                                    <button className="btn btn-warning btn-sm text-white">Edit</button>
+                                                </div>
+                                            </form>
+                                        </dialog>
+                                    </td>
                                 </tr>
                             ))}
                         </tbody>
